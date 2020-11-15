8d30b917c8d7e2913bd9a9f470f5fd4d2c0bf593
Wares Fazelyar <waresfazelyar@Waress-MacBook-Pro.local>
2020-11-14 21:30:07 -0500

    added hardcoded data and a function to retrieve title from that data. next step will be to render the data dynamically

--- src/Item.js
+++ src/Item.js
@@ -12,13 +12,23 @@ export default function item() {
             src="images/qabuli.png"
             alt=""
           />
+          {/* <div className="chef-info">
+            <img 
+              className="chef-thumbnail"
+              src="images/chef.png"
+              alt=""
+            />
+            <p className="chef-name">Chef George</p>
+          </div> */}
         </div>
 
-        <ul className="item-details">
-          <p className="title">Qabuli Palaw Palooza</p>
-          <p>$45/ person</p>
+        <div className="item-details">
+          <div className="item-header">
+            <p className="title">Qabuli Palaw Palooza</p>
+            <p>$45/ person</p>
+          </div>
           <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non laoreet ipsum, id rutrum dui. Donec pretium lacus libero, at pretium justo consectetur ornare. Integer at dolor nibh.</p>
-        </ul>
+        </div>
 
         <div className="item-button">
           <button className="select-item">Select</button>
