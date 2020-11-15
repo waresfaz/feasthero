a123ddeeee8865e16a015a555c031e922da7b0ab
Wares Fazelyar <waresfazelyar@Waress-MacBook-Pro.local>
2020-11-14 19:24:15 -0500

    completed v1 skeleteal version of item card

--- src/Item.js
+++ src/Item.js
@@ -5,7 +5,25 @@ export default function item() {
     <main>
 
       <div className="item">
-        <p>Helo from item</p>
+
+        <div className="item-image">
+          <img 
+            className="thumbnail"
+            src="images/qabuli.png"
+            alt=""
+          />
+        </div>
+
+        <ul className="item-details">
+          <p className="title">Qabuli Palaw Palooza</p>
+          <p>$45/ person</p>
+          <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non laoreet ipsum, id rutrum dui. Donec pretium lacus libero, at pretium justo consectetur ornare. Integer at dolor nibh.</p>
+        </ul>
+
+        <div className="item-button">
+          <button className="select-item">Select</button>
+        </div>
+
       </div>
 
     </main>
