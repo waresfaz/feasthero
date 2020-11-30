0b7c3f9e68c7a328673832b32f86caedc6178061
Wares Fazelyar <waresfazelyar@Waress-MacBook-Pro.local>
2020-11-29 19:02:05 -0500

    cleaned up code, added form to modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,58 +1,3 @@
-// // import PropTypes from 'prop-types';
-
-// export default function modal(props) {
-
-//   console.log(props.show)
-
-//   // Render nothing if the "show" prop is false
-//   if(props.show === false) {
-//     return null;
-//   }
-
-//   // The gray background
-//   const backdropStyle = {
-//     position: 'fixed',
-//     top: 0,
-//     bottom: 0,
-//     left: 0,
-//     right: 0,
-//     backgroundColor: 'rgba(0,0,0,0.3)',
-//     padding: 50
-//   };
-
-//   // The modal "window"
-//   const modalStyle = {
-//     backgroundColor: '#fff',
-//     borderRadius: 5,
-//     maxWidth: 500,
-//     minHeight: 300,
-//     margin: '0 auto',
-//     padding: 30
-//   };
-
-//   return (
-//     <div className="backdrop" style={{backdropStyle}}>
-//       <div className="modal" style={{modalStyle}}>
-//         This is showing now so it's set to true
-
-//         <div className="footer">
-//           <button onClick={props.onClose}>
-//             Close
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// // Modal.propTypes = {
-// //   onClose: PropTypes.func.isRequired,
-// //   show: PropTypes.bool,
-// //   children: PropTypes.node
-// // };
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Modal.css';
@@ -67,9 +12,9 @@ const Modal = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <p>
-          Hello, I'm a modal.
-        </p>
+        <form>
+          <input type="text" name="firstName" placeholder="First Name" />
+        </form>
       </div>
     </div>
   </React.Fragment>, document.body
