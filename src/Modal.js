1afed35a7f1dadc58dc25f99442de097cdd2a532
Wares Fazelyar <waresfazelyar@Waress-MacBook-Pro.local>
2020-12-24 14:54:45 -0500

    added changes to modal. next step is to set up backend and database

--- src/Modal.js
+++ src/Modal.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './Modal.css';
 
 //object destrcutring used here to access props
-const Modal = ({ isShowing, hide, duration, chef }) => isShowing ? ReactDOM.createPortal(
+const Modal = ({ isShowing, hide, chef }) => isShowing ? ReactDOM.createPortal(
   <React.Fragment>
     <div className="modal-overlay"/>
     <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
@@ -17,6 +17,12 @@ const Modal = ({ isShowing, hide, duration, chef }) => isShowing ? ReactDOM.crea
           <img className="chefPhoto" src={chef.photo} alt="" />
           <p>Chef: {chef.name} </p>
           <p>{chef.bio} </p> 
+          <form>
+            <p>How many people are you booking for?
+              <input type="number" className="booking-size" min="8" max="20" placeholder="Limit 8-20" />
+              <input type="button" className="submit-form" value="Submit" />
+            </p>
+          </form>
         </div>
       </div>
     </div>
