28fadf6f2a799fe92c2cd58b80095616597c8a67
Prachita Nayak <pnayak@measureone.com>
2021-03-31 15:02:10 +0530

    changes to service file

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,37 +1,7 @@
 import axios from "axios";
 const getClassAPi = async () => {
-  const data = await axios.get(`http://localhost:3001/classes`).then((res) => {
-    return res;
-  });
-
-  return data;
-};
-
-const bookClassAPI = async (data) => {
-  let bookingResult = await axios
-    .post(`http://localhost:3001/booking`, { data })
-    .then((res) => {
-      return res.data;
-    });
-  return bookingResult;
-};
-
-const paymentAPI = async (data) => {
-  var bodyFormData = new FormData();
-  bodyFormData.append("ps_store_id", "CVQNQtore1");
-  bodyFormData.append("hpp_key", "hpB9RYVYPULN");
-  bodyFormData.append("charge_total", "40.00");
-  let bookingResult = await axios
-    .post(`https://esqa.moneris.com/HPPDP/index.php`, { data })
-    .then((res) => {
-      return res.data;
-    });
-  return bookingResult;
-};
-
-const getScheduleAPI = async (class_id) => {
   const data = await axios
-    .get(`http://localhost:3001/schedule/` + class_id)
+    .get(`https://feasthero.herokuapp.com/classes`)
     .then((res) => {
       return res;
     });
@@ -39,4 +9,35 @@ const getScheduleAPI = async (class_id) => {
   return data;
 };
 
-export { getClassAPi, bookClassAPI, paymentAPI, getScheduleAPI };
+const bookClassAPI = async (data) => {
+  let bookingResult = await axios
+    .post(`https://feasthero.herokuapp.com/booking`, { data })
+    .then((res) => {
+      return res.data;
+    });
+  return bookingResult;
+};
+
+const cancelBookingAPI = async (data) => {
+  let bookingResult = await axios
+    .post(`https://feasthero.herokuapp.com/payment`, {
+      response_order_id: data,
+      is_cancelled: true,
+    })
+    .then((res) => {
+      return res;
+    });
+  return bookingResult;
+};
+
+const getScheduleAPI = async (class_id) => {
+  const data = await axios
+    .get(`https://feasthero.herokuapp.com/schedule/` + class_id)
+    .then((res) => {
+      return res;
+    });
+
+  return data;
+};
+
+export { getClassAPi, bookClassAPI, cancelBookingAPI, getScheduleAPI };
