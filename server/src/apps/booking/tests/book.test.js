c079e215e526a02d7c2e80622f492eb406bf6ddd
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-07-26 11:14:23 -0400

    chef images

--- server/src/apps/booking/tests/book.test.js
+++ server/src/apps/booking/tests/book.test.js
@@ -3,21 +3,11 @@ const stripe = require('stripe')(settings.stripe.SECRET_KEY);
 
 
 describe('book class', function () {
-    it('creating a charge', async function () {
-        await createCharge();
+    it('booking a class', async function () {
+        await createCardToken();
     });
 });
 
-
-async function createCharge() {
-    return await stripe.charges.create(
-        {
-            amount: 1000,
-            currency: 'cad',
-            source: (await createCardToken()).id,
-            description: 'unit test payment',
-        })
-}
 
 async function createCardToken() {
     return await stripe.tokens.create({
